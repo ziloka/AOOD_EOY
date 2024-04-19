3ccc9d4e65ef60972a57a1c7c8767948514ea470
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-04-19 17:37:06 -0400

    dynamically draw every biome

--- src/consts.py
+++ src/consts.py
@@ -1,6 +1,12 @@
+from enum import Enum
+
 # maximum 12 tiles in each row (keep 4:3 ratio)
 TILES_COLUMN = 32
 TILES_ROW = 24
 
 # in each spritesheet, sprites are 16 by 16
 TILE_SIZE = 16
+
+class biomes(Enum):
+    dirt = 0.009
+    grass = 0.002
--- src/index.py
+++ src/index.py
@@ -2,6 +2,7 @@
 import random
 import pygame
 import json
+import sys
 from pygame.locals import *
 from perlin_noise import PerlinNoise
 from consts import *
@@ -19,32 +20,32 @@ noise = PerlinNoise(octaves=6, seed=SEED)
 xpix, ypix = TILES_COLUMN, TILES_ROW
 noise_map = [[noise([i/xpix, j/ypix]) for j in range(xpix)] for i in range(ypix)]
 tile_map = [[0 for j in range(xpix)] for i in range(ypix)]
-print(f"noise_map: # rows: {len(noise_map)}, # cols: {len(noise_map[0])}")
-print(f"RESIZE TILE: {RESIZE_TILE}")
 
 def draw_terrain():
-  for i in range(0, TILES_ROW):
-    for j in range(0, TILES_COLUMN):
-      cropped = pygame.Surface((TILE_SIZE, TILE_SIZE))
-      column = noise_map[i][j] # the smaller the number is the more likely it is going to show up
-      
-      if column >= 0.003:
-        if tile_map[i][j] == 0:
-          data = sprite_metadata["dirt"]
-          x = random.randint(0, data["columns"]-1) * TILE_SIZE # 1 presenting the number of rows in dirt sprite - 1
-          y = random.randint(0, data["rows"]-1) * TILE_SIZE
-          tile_map[i][j] = (x, y)
-        cropped.blit(pygame.image.load("assets/dirt.png"), (0, 0), (*tile_map[i][j], TILE_SIZE, TILE_SIZE))
-        cropped = pygame.transform.scale(cropped, (RESIZE_TILE, RESIZE_TILE))
-        screen.blit(cropped, (i * RESIZE_TILE, j * RESIZE_TILE))
+    for i in range(0, TILES_ROW):
+        for j in range(0, TILES_COLUMN):
+            cropped = pygame.Surface((TILE_SIZE, TILE_SIZE))
+            column = noise_map[i][j] # the smaller the number is the more likely it is going to show up
+            # https://stackoverflow.com/a/74592123
+            for biome in biomes:
+                if column >= biome.value:
+                    if tile_map[i][j] == 0:
+                        data = sprite_metadata[biome.name]
+                        x = random.randint(0, data["columns"]-1) * TILE_SIZE
+                        y = random.randint(0, data["rows"]-1) * TILE_SIZE
+                        tile_map[i][j] = (x, y)
+                    cropped.blit(pygame.image.load(f"assets/{biome.name}.png"), (0, 0), (*tile_map[i][j], TILE_SIZE, TILE_SIZE))
+                    cropped = pygame.transform.scale(cropped, (RESIZE_TILE, RESIZE_TILE))
+                    screen.blit(cropped, (i * RESIZE_TILE, j * RESIZE_TILE))
 
 while True:
-  clock.tick(60)
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      pygame.quit()
-    if event.type == pygame.WINDOWRESIZED:
-      width, height = screen.get_width(), screen.get_height()
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.WINDOWRESIZED:
+            width, height = screen.get_width(), screen.get_height()
 
-  draw_terrain()
-  pygame.display.flip()
+    draw_terrain()
+    pygame.display.flip()
